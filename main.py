13d0b63ce94ec94a44d8fd71c74167675b7e52b4
neto-works <clodoaldo.brtp4@gmail.com>
2024-07-15 19:21:22 -0300

    dev: identificado SO e pegado drive para execução

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+import os
+import time
+import platform
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
+chrome_driver_path = ""
+
+def identificar_sistema():
+    sistema = platform.system()
+    if sistema == "Windows":
+        return os.path.abspath(os.getcwd()) + "/chromedriver_win32"
+    elif sistema == "Linux":
+        return os.path.abspath(os.getcwd()) + "/chromedriver_linux64"
+    else:
+        print("Sistema operacional não reconhecido:", sistema)
+
+chrome_driver_path = identificar_sistema()
 html = urlopen("https://www.linkedin.com/mynetwork/grow/")
 bs = BeautifulSoup(html.read(),"html.parser")
-print(bs)
+#print(bs)
+print(chrome_driver_path)
